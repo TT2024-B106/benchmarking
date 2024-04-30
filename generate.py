40f434afa34c54bcc5c071abe6375e5af28be4bd
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-04-29 20:41:12 -0600

    Implemented generation of geojson files

--- generate.py
+++ generate.py
@@ -34,7 +34,7 @@ def geojson(features: int) -> object:
 
     return feature_collection
 
-def geojson_file(pairs: int, target_dir="generated") -> None:
+def geojson_file(pairs: int, target_dir="generated", filename="") -> None:
     """
     Generate a geojson file with a given number of pair points.
 
@@ -43,10 +43,33 @@ def geojson_file(pairs: int, target_dir="generated") -> None:
     """
     # Create the directory if it doesn't exist
     os.makedirs(target_dir, exist_ok=True)
+
     # Create the full path to the file
-    nowstr = datetime.now().strftime("%y%m%d%H%M%S")
-    filename = "t" + nowstr + "_p" + str(pairs) + ".json"
+    if filename == "":
+        nowstr = datetime.now().strftime("%y%m%d%H%M%S")
+        filename = "t" + nowstr + "_p" + str(pairs) + ".json"
+    elif filename[-5:] != ".json":
+        filename += ".json"
+
     filepath = os.path.join(target_dir, filename)
 
     with open(filepath, "w") as file:
         json.dump(geojson(pairs), file)
+
+def geojson_files(files: int, target_dir="generated") -> None:
+    """
+    Generate a number of geojson files that will create from 1 to `files`
+    number of features for each file. I.e.: 1.json, 2.json, ..., `files`.json;
+    where these numbers will represent the number of features that each file
+    has.
+
+    :param files: The number of files to be created.
+    :param target_dir str: The target directory where it will be saved.
+    """
+    # Create directories if they do not exist
+    nowstr = datetime.now().strftime("%y%m%d%H%M%S")
+    dirpath = target_dir + "/" + nowstr
+    os.makedirs(dirpath, exist_ok=True)
+
+    for i in range(1, files+1):
+        geojson_file(i, dirpath, str(i))
