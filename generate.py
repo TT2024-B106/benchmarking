a891847243307beb187c2475f26c09c608f5e919
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-04-29 20:17:43 -0600

    Implemented geojson file generation

--- generate.py
+++ generate.py
@@ -1,5 +1,8 @@
+import os
 import json
 import random
+
+from datetime import datetime
 
 def trajectory(size: int) -> list[object]:
     """Generate trajectory as a collection of Points (features).
@@ -30,3 +33,20 @@ def geojson(features: int) -> object:
     }
 
     return feature_collection
+
+def geojson_file(pairs: int, target_dir="generated") -> None:
+    """
+    Generate a geojson file with a given number of pair points.
+
+    :param pairs: The number of pair points (tuples).
+    :param target_dir str: The target directory where it will be saved.
+    """
+    # Create the directory if it doesn't exist
+    os.makedirs(target_dir, exist_ok=True)
+    # Create the full path to the file
+    nowstr = datetime.now().strftime("%y%m%d%H%M%S")
+    filename = "t" + nowstr + "_p" + str(pairs) + ".json"
+    filepath = os.path.join(target_dir, filename)
+
+    with open(filepath, "w") as file:
+        json.dump(geojson(pairs), file)
